d09b105231599f8a1a5356a976763cf28691ba01
barlesh <barlesh8@gmail.com>
2017-07-16 21:23:06 +0300

    with receiver

--- receiver.py
+++ receiver.py
@@ -1,14 +1,14 @@
 import socket
 
-UDP_IP_FROM_FFMPEG = "127.0.0.1"
-UDP_PORT_FROM_FFMPEG = 1234
+UDP_IP_FROM_SENDER = "127.0.0.1"
+UDP_PORT_FROM_SENDER = 5678
 
-UDP_IP_TO_RECEIVER = "127.0.0.1"
-UDP_PORT_TO_RECEIVER = 5678
+UDP_IP_TO_FFMPEG = "127.0.0.1"
+UDP_PORT_TO_FFMPEG = 9012
 
 sockin = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sockin.bind((UDP_IP_FROM_FFMPEG, UDP_PORT_FROM_FFMPEG))
+sockin.bind((UDP_IP_FROM_SENDER, UDP_PORT_FROM_SENDER))
 
 sockout = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
@@ -16,6 +16,5 @@ sockout = socket.socket(socket.AF_INET,  # Internet
 while True:
     data, addr = sockin.recvfrom(2048) # buffer size is 1024 bytes
     # print "received message:", data
-    print "current stream length is" + str(len(data))
-    sockout.sendto(data, (UDP_IP_TO_RECEIVER, UDP_PORT_TO_RECEIVER))
-
+    # print "current stream length is" + str(len(data))
+    sockout.sendto(data, (UDP_IP_TO_FFMPEG, UDP_PORT_TO_FFMPEG))
